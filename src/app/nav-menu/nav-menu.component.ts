78e1e369c6f91fd4282cab2df152a6bc2542d64e
ashokbrolia <32103544+ashokbrolia@users.noreply.github.com>
2020-08-13 22:33:34 -0400

    Changes made to link pages

---
+++ src/app/nav-menu/nav-menu.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-menu',
+  templateUrl: './nav-menu.component.html',
+  styleUrls: ['./nav-menu.component.css']
+})
+export class NavMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  isExpanded = false;
+
+  collapse() {
+    this.isExpanded = false;
+  }
+
+  toggle() {
+    this.isExpanded = !this.isExpanded;
+  }
+
+}
