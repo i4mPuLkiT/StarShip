846ab726cbecb0fbf9aa4e18650394ebef16abe8
Pulkit <69210716+i4mPuLkiT@users.noreply.github.com>
2020-08-14 15:05:12 -0400

    Update tables.component.ts

--- src/app/tables/tables.component.ts
+++ src/app/tables/tables.component.ts
@@ -6,10 +6,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tables.component.css']
 })
 export class TablesComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+ngOnInit() {
   }
+   public transactions: any;
+   constructor(){
+       this.transactions = [
+         {
+        "id": 1,
+        "name": "Ashok 1",
+        "dbt": 500,
+        "crt": 0
+         },
+         {
+        "id": 2,
+        "name": "Ashok 2",
+        "dbt": 890,
+        "crt": 0
+         },
+         {
+        "id": 3,
+        "name": "Ashok 3",
+        "drt": 0,
+        "crt": 4000
+         },
+         {
+        "id": 4,
+        "name": "Ashok 4",
+        "dbt": 0,
+        "crt": 5000
+         },
+       ]
+       
+   }
+
+  
 
 }
