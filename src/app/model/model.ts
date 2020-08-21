3cfed34bf4b1195f40a577549df6dedee0e184b7
i4mPuLkiT <pulkitbrolia@gmail.com>
2020-08-21 20:13:26 +0530

    1 file modified, 1 added

---
+++ src/app/model/model.ts
@@ -0,0 +1,16 @@
+export class Appuser {
+   id: number;
+    name: string;
+        email: string;
+        phone: string;
+        password: string;
+        isAuthenticate: boolean;
+}
+export class Transaction {
+   id: number;
+        date: string;
+        debit: number;
+        credit: number;
+        description: string;
+        user: Appuser;
+}
